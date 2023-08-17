820558b5636eca38ba91681d02d69358c3c65726
Aditya Chaudhary <aditya.chaudhary1@ugrocapital.com>
2023-08-17 18:46:02 +0530

    Synchronized Block and Thread pool

---
+++ Day3/src/Worker.java
@@ -0,0 +1,69 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Worker {
+    private Random random=new Random();
+    private List<Integer>list1=new ArrayList<Integer>();
+    private List<Integer>list2=new ArrayList<Integer>();
+    private Object lock1=new Object();
+    private Object lock2=new Object();
+    public void stageOne()  {
+        synchronized (lock1){
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            list1.add(random.nextInt(100));
+        }
+    }
+    public  void stageTwo(){
+        synchronized (lock2){
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            list2.add(random.nextInt(100));
+        }
+
+    }
+    public void process(){
+        for (int i = 0; i <1000 ; i++) {
+            stageOne();
+            stageTwo();
+        }
+    }
+    public  void main() {
+
+        System.out.println("Starting !");
+        long start=System.currentTimeMillis();
+      Thread t1=  new Thread(new Runnable() {
+            @Override
+            public void run() {
+                process();
+            }
+        },"Thread1");
+      Thread t2=new Thread(new Runnable() {
+          @Override
+          public void run() {
+              process();
+          }
+      },"Thread2");
+      t1.start();
+      t2.start();
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        long end=System.currentTimeMillis();
+        System.out.println("Time Taken"+(end-start));
+        System.out.println("List 1 size"+list1.size());
+        System.out.println("List 2 size"+list2.size());
+
+    }
+}
